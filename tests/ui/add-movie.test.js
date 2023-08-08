24a39573d69e3d480de650e423b9b78d9a160013
VasilMG <vassil.manolov@abv.bg>
2023-08-08 21:05:52 +0300

    correcting yml

--- tests/ui/add-movie.test.js
+++ tests/ui/add-movie.test.js
@@ -1,6 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 
+
 test('Check add movie page', async ({ page }) => {
     await page.goto('https://vasilmg-test-exam-prep.onrender.com/add-movie');
     const form = await page.$('form');
