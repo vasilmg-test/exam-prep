95410b0aff84c4a41a8e1abf79189278386c7cc3
VasilMG <vassil.manolov@abv.bg>
2023-08-08 20:24:07 +0300

    change tests and pi

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -1,7 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check about page', async ({ page }) => {
-  await page.goto('http://localhost:8080/about');
+  // await page.goto('http://localhost:8080/about');
+  await page.goto('https://vasilmg-test-exam-prep.onrender.com/about');
   const heading = await page.$('h1');
   const text = await heading.textContent();
   expect(text).toBe('About');
